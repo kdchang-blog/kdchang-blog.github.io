2c0cadaa40f687e9ff20fb8f489a4c76a5efe1d8
KD Chang <kdchang.cc@gmail.com>
2026-01-10 21:52:24 +0800

    feat: update local search config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -97,7 +97,7 @@ const config: Config = {
         // language
         language: ["en", "zh"],
         // highlight term
-        highlightSearchTermsOnTargetPage: true,
+        highlightSearchTermsOnTargetPage: false,
         // docs route base path
         docsRouteBasePath: "/resources",
         blogRouteBasePath: "/blog",
